9c0af764c581b7d656f1308fc9852a130c8dcd38
Ivan Zlatev <ivan@ivanz.com>
2015-09-06 20:19:22 +0100

    Check-in

---
+++ AnotherClassLibrary/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherClassLibrary
+{
+    public class Car
+    {
+
+        private string _model;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+
+            int i = 0;
+        }
+    }
+}
---
+++ RoslynAnalysersUsageSample/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynAnalysersUsageSample
+{
+    public class Car
+    {
+
+        private string _model;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
